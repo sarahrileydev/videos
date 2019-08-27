976cb283ddbeead48e381ac9b82a5dc907671f8f
sriley <sarahriley.dev@gmail.com>
2019-08-26 20:21:36 -0400

    setting up api key

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAwrG19tkbHJI2IADOi4xd0ODv9PAww-zw";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 5,
-    key: KEY
+    key: process.env.REACT_APP_YOUTUBE_KEY
   }
 });
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = e => {
     e.preventDefault();
-    //needs callback to parent
+    this.props.onFormSubmit(this.state.term)
   };
 
   render() {
