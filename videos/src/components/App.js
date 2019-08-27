424cf750818ee341eb53e859084782c8bf43fe95
sriley <sarahriley.dev@gmail.com>
2019-08-27 10:00:30 -0400

    rendering thumbnails of videos

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 import youtube from "../api/youtube";
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get("/search", {
+  state = {
+    videos: []
+  };
+  onTermSubmit = async term => {
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
+    this.setState({ videos: response.data.items });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
